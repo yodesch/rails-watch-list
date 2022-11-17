1f97d20666a907da6d2268962ff0b7b24804613c
Yoann DESCHAUX <yoanndeschaux@gmail.com>
2022-11-18 00:51:41 +0100

    watch list v0

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :destroy]
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
